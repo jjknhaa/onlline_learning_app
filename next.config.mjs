da3c0f471d1274620714d3684a5a4715176e0074
jjknhaa <suvobiswassuvo01@gmail.com>
2026-06-26 23:34:13 +0600

    Initial clone for mobile app development

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Tells Next.js to generate static files into the 'out' folder
   images: {
+    unoptimized: true, // Required for static exports & mobile wrappers
     remotePatterns: [
       {
         protocol: 'https',
