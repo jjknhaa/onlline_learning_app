260d59c05bf3af5459bd13947f72c05e1b5de708
jjknhaa <suvobiswassuvo01@gmail.com>
2026-06-27 00:25:50 +0600

    feat: add android folder and github actions apk workflow

---
+++ android/app/src/main/java/com/jjknhaa/onlinelearning/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jjknhaa.onlinelearning;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Tells Next.js to generate static files into the 'out' folder
+ // output: 'export', // Tells Next.js to generate static files into the 'out' folder
   images: {
     unoptimized: true, // Required for static exports & mobile wrappers
     remotePatterns: [
